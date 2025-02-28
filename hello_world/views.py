de9a881d1e31e0d109099cae180f5edb2b6ea783
Rick-8 <npopbwls@students.codeinstitute.net>
2025-02-28 18:54:03 +0000

    final getting started code

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the hello_world index.")
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(f"Request method: {request.method}")
